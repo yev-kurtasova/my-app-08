76c2a7a7017fbb0be5c40eee2e5c92b53b1e83bf
yev-kurtasova <eugenia.kurtasova@gmail.com>
2024-01-11 14:36:58 +0200

    create my-app-08 home work

---
+++ src/store/counterSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    count: 0
+}
+
+export const counterSlice = createSlice({
+    name: "counterValue",
+    initialState,
+    reducers: {
+        increment: (state) => {
+            state.count += 1;
+        },
+        decrement: (state) => {
+            state.count -= 1;
+        }
+    }
+})
+
+export const { increment, decrement } = counterSlice.actions;
+
+export default counterSlice.reducer;
